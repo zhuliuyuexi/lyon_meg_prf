2635517a24b77a3c812a701c408c0fd8a94a14e4
Tomas Knapen <tknapen@gmail.com>
2019-08-01 22:09:16 +0200

    updated mriqc singularity path

--- run_slurm_singularity.py
+++ run_slurm_singularity.py
@@ -90,7 +90,7 @@ if args.mriqc_subject:
 #SBATCH -t 1:00:00 -N 1
 
 PYTHONPATH="" singularity run -B /mnt/data/rasa,/mnt/data/rasa/prf_lyon/tmp:/opt/templateflow \
-/mnt/data/rasa/software/mriqc_0.14.2.simg \
+/mnt/data/rasa/software/mriqc-0.14.2.simg \
 /mnt/data/rasa/prf_lyon/bids/ /mnt/data/rasa/prf_lyon/derivatives/out/ participant \
 --participant-label sub-$SJ_NR --n_procs 15 -m bold --verbose-reports --mem_gb 32 \
 --ants-nthreads 15  --skip_bids_validation -w /mnt/data/rasa/prf_lyon/tmp
@@ -126,7 +126,7 @@ if args.mriqc_group:
 #SBATCH -t 1:00:00 -N 1
 
 PYTHONPATH="" singularity run -B /mnt/data/rasa,/mnt/data/rasa/prf_lyon/tmp:/opt/templateflow \
-/mnt/data/rasa/software/poldracklab_mriqc_latest-2019-04-05-f2009956414a.simg \
+/mnt/data/rasa/software/mriqc-0.14.2.simg \
 /mnt/data/rasa/prf_lyon/bids/ /mnt/data/rasa/prf_lyon/derivatives/out/ participant \
 --n_procs 15 -m bold --verbose-reports --mem_gb 32 --ants-nthreads 15  --skip_bids_validation -w /mnt/data/rasa/prf_lyon/tmp
 
