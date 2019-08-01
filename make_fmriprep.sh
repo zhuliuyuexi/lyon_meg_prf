25bc04bfc94e8dbdf00588e7d15db195a21e88f3
Tomas Knapen <tknapen@gmail.com>
2019-08-01 22:00:00 +0200

    updated fmriprep generation script to work

--- make_fmriprep.sh
+++ make_fmriprep.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 VERSION=${1?Error: give version of fmriprep you want to build!}
-docker run --privileged -t --rm \
--v /var/run/docker.sock:/var/run/docker.sock \
--v /home/shared/software/bids_apps:/output \
-singularityware/docker2singularity \
-poldracklab/fmriprep:$VERSION
+#docker run --privileged -t --rm \
+#-v /var/run/docker.sock:/var/run/docker.sock \
+#-v /home/shared/software/bids_apps:/output \
+#singularityware/docker2singularity \
+#poldracklab/fmriprep:$VERSION
+
+singularity build fmriprep-$VERSION.simg docker://poldracklab/fmriprep:$VERSION
